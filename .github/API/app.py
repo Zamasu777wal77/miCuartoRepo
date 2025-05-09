7feaab229d3a310963ce78799b2d811df1ee2102
Walter Alejandro Valdiviezo <alejoooo4848@gmail.com>
2025-05-08 22:49:04 -0300

    cccccc

--- .github/API/app.py
+++ .github/API/app.py
@@ -3,14 +3,26 @@ from typing import Union
 from fastapi import FastAPI
 from pydantic import BaseModel
 
+
+
 app = FastAPI()
+
+
 
 @app.get("/familia")
 def get_familia():
     rows = ["Amin", "Marce", "Miranda"]
     return rows
 
+
+
 @app.get("/superheroesDC")
+def get_superheroes():
+    rows = ["Superman", "Batman", "Flash", "Linterna Verde", "Mujer maravilla", "Aquaman", "Shazam", "Cyborg"]
+    return rows
+
+
+@app.get("/superheroesDCdcDCdcxxxxx")
 def get_superheroes():
     rows = ["Superman", "Batman", "Flash", "Linterna Verde", "Mujer maravilla", "Aquaman", "Shazam", "Cyborg"]
     return rows
